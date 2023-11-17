a843584d0983e441f282113ae1a6e5643a49c5bb
Che Kulhan <che.kulhan@gmail.com>
2023-11-17 15:35:28 +0000

    AlertButton

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+function AlertButton({ title, message }) {
+  return <button onClick={() => alert(message)}>{title}</button>;
+}
+
 export default function Quiz() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -12,6 +16,10 @@ export default function Quiz() {
     setLastName(e.target.value);
   }
 
+  function handle_Click() {
+    alert("you clicked me!");
+  }
+
   const fullName = firstName + " " + lastName;
 
   return (
@@ -21,6 +29,9 @@ export default function Quiz() {
       <input value={firstName} onChange={handleFirstName_Change}></input>
       <input value={lastName} onChange={handleLastName_Change}></input>
       <p>You are typing: {fullName}</p>
+      <button onClick={handle_Click}>Click me - {firstName}</button>
+      <AlertButton title="Here I am" message="You clicked me" />
+      <AlertButton title="I amnot heream" message="And me" />
     </>
   );
 }
