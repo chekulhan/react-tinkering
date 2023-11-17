0c885e0aecd03716d08829a2fced6b93aef63be2
Che Kulhan <che.kulhan@gmail.com>
2023-11-17 15:25:35 +0000

    First and LastName questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,26 @@
-function Quiz() {
-  return <div>Quiz</div>;
-}
+import { useState } from "react";
 
-export default Quiz;
+export default function Quiz() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFirstName_Change(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastName_Change(e) {
+    setLastName(e.target.value);
+  }
+
+  const fullName = firstName + " " + lastName;
+
+  return (
+    <>
+      <h1>Quick quiz</h1>
+      <p>Question goes here...</p>
+      <input value={firstName} onChange={handleFirstName_Change}></input>
+      <input value={lastName} onChange={handleLastName_Change}></input>
+      <p>You are typing: {fullName}</p>
+    </>
+  );
+}
